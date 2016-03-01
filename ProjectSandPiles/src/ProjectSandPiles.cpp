26ea52559388ee22f6f029707a34afddf634f9e5
iAmtheSystem <simonreichel@yahoo.de>
2016-03-01 15:32:37 +0100

    First cout

---
+++ ProjectSandPiles/src/ProjectSandPiles.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ProjectSandPiles.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "S A N D   P I L E S" << endl; !
+	return 0;
+}
